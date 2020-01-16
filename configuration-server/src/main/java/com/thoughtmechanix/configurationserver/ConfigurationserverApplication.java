0723d5743f87e770bbc5ee601efa34e91473ada2
Sultan Kossayev <sultan.kossayev@gmail.com>
2020-01-16 11:44:51 +0300

    add config server files generated by spring initialzr

---
+++ configuration-server/src/main/java/com/thoughtmechanix/configurationserver/ConfigurationserverApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.configurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationserverApplication.class, args);
+	}
+
+}
