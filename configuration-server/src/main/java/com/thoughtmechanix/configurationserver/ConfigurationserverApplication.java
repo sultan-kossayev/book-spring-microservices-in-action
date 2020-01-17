4994762415aa0284cbda5043db0edcffa453f32d
Sultan Kossayev <sultan.kossayev@gmail.com>
2020-01-17 13:16:08 +0300

    connect licenses service to spring config

--- configuration-server/src/main/java/com/thoughtmechanix/configurationserver/ConfigurationserverApplication.java
+++ configuration-server/src/main/java/com/thoughtmechanix/configurationserver/ConfigurationserverApplication.java
@@ -2,8 +2,10 @@ package com.thoughtmechanix.configurationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigurationserverApplication {
 
 	public static void main(String[] args) {
