33f0a37290fe2cbdb56c96b2b96fc770b199c429
Sultan Kossayev <sultan.kossayev@gmail.com>
2020-01-23 11:10:42 +0300

    add zuul server project files generated by spring initializr

---
+++ zuulserver/src/main/java/com/thoughtmechanix/zuulserver/ZuulserverApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulserverApplication.class, args);
+	}
+
+}
---
+++ zuulserver/src/test/java/com/thoughtmechanix/zuulserver/ZuulserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
